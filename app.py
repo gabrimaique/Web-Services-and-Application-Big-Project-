1874c69dfce430f34a2a26a62241aa6a6c7b9de7
gabrimaique <98279370+gabrimaique@users.noreply.github.com>
2024-05-14 13:39:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:sarab%40123@localhost/pubs_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:gabriel%40123@localhost/pubs_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
